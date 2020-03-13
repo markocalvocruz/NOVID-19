9567ac1bfc65f3089efbc53395876d74f681f30e
Marko Calvo-Cruz <markocalvocruz@gmail.com>
2020-03-13 18:51:57 -0500

    Initial Commit

---
+++ COVID19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  COVID19
+//
+//  Created by Mark Calvo-Cruz on 3/13/20.
+//  Copyright © 2020 Mark Calvo-Cruz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
